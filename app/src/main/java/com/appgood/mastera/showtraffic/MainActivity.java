895bdb1a7a70465eaf498ba40361f4703bd35eef
book <tirajitr@gmail.com>
2016-08-04 14:57:39 +0700

    Sound Effect

--- app/src/main/java/com/appgood/mastera/showtraffic/MainActivity.java
+++ app/src/main/java/com/appgood/mastera/showtraffic/MainActivity.java
@@ -1,13 +1,41 @@
 package com.appgood.mastera.showtraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.listView);
+        button = (Button) findViewById(R.id.button);
+
+        //Buttom controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.elephant);
+                mediaPlayer.start();
+
+            }   //  onClick
+        });
+
+
+    }   // Main Method
+
+
+}    // Main Class นี่คือคลาสหลัก
